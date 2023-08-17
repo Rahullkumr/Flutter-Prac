1056d4256ddd7c97d78ce607f225bbff34182908
Rahul Kumar <kumarrahul771996@gmail.com>
2023-08-17 23:10:43 +0000

    added image from the internet

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text("hello world"),
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: const Center(  
+            child: Text("I Am Rich"),
+          ),
+          backgroundColor: Colors.grey[900],
+        ),
+        body: const Center(
+          child: Image(
+            // acts as an empty frame of any real world photo
+            image: NetworkImage('https://picsum.photos/id/28/300'),
+          ),
+        ),
       ),
     ),
   );
