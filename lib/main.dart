317cc25e68f48b96efcaef8a8a636cd6315d1459
Rahul Kumar <kumarrahul771996@gmail.com>
2023-08-19 15:32:32 +0000

    little changes

--- lib/main.dart
+++ lib/main.dart
@@ -6,26 +6,44 @@ void main() {
   );
 }
 
-class MyApp extends StatelessWidget { 
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.grey[900],
         body: SafeArea(
-          child: Container(
-            color: Colors.red,
-            height: 100.0,
-            width: 100.0,
-            margin: const EdgeInsets.all(10.0),
-            padding: const EdgeInsets.all(20.0),
-            child: const Text("hello"),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                color: Colors.lightBlue,
+                height: 100.0,
+                width: double.infinity,
+                child: const Text("container 1"),
+              ),
+              const SizedBox(
+                height: 30.0,
+              ),
+              Container(
+                color: Colors.lightGreen,
+                height: 100.0,
+                width: 300.0,
+                child: const Text("container 2"),
+              ),
+              Container(
+                color: Colors.indigoAccent,
+                height: 100.0,
+                width: 200.0,
+                child: const Text("container 3"),
+              )
+            ],
           ),
         ),
       ),
     );
   }
 }
-
