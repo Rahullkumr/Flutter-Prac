e679c0d4de7c484adcc6efcc423fdc9107942500
Rahul Kumar <kumarrahul771996@gmail.com>
2023-08-19 13:44:21 +0000

    learnt about Container

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,30 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget { 
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-          title: const Center(  
-            child: Text("I Am Rich"),
-          ),
-        ),
-        body: const Center(
-          child: Image(
-            // acts as an empty frame where an image will be kept
-            image: AssetImage('images/diamond.png'),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Container(
+            color: Colors.red,
+            height: 100.0,
+            width: 100.0,
+            margin: const EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(20.0),
+            child: const Text("hello"),
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
+
