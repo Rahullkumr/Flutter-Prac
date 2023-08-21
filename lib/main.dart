67fcfb7259a89259cf10e361435de9cda0bbb0e5
Rahul Kumar <kumarrahul771996@gmail.com>
2023-08-21 21:43:50 +0000

    settings changed

--- lib/main.dart
+++ lib/main.dart
@@ -11,28 +11,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                color: Colors.red,
-                width: 100,
-              ),
-              const CircleAvatar(
-                radius: 100.0,
-                backgroundColor: Colors.yellow,
-              ),
-              Container(
-                color: Colors.blue,
-                // height: 70.0,
-                width: 100,
-              ),
-            ],
+          child: CircleAvatar(
+            backgroundColor: Colors.yellow,
+            radius: 100,
           ),
         ),
       ),
