faec6320715f209b294ece1925b3766544f5a5b2
Rahul Kumar <kumarrahul771996@gmail.com>
2023-08-21 17:52:14 +0000

    given details to change access mode to executable

--- run.sh
+++ run.sh
@@ -1,1 +1,13 @@
 flutter run -d web-server --web-hostname 0.0.0.0 --web-port 3000
+
+# change the permission to executable
+
+# chmod u+x run.sh
+
+# chmod is a command in Linux and other Unix-like operating systems that allows to change the permissions (or access mode) of a file or directory.
+# u+x means give the user executable permission
+# run.sh is the file name
+
+
+# finally run the script
+# ./run.sh 
