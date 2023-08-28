f3e99c03a1aa7037c21eaa33f4f82c4462e2db95
Rahul Kumar <kumarrahul771996@gmail.com>
2023-08-28 19:55:50 +0000

    learnt Divider widget

--- lib/main.dart
+++ lib/main.dart
@@ -15,86 +15,81 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: Column(
-              children: [
-                CircleAvatar(
-                  radius: 50,
-                  backgroundColor: Colors.grey[900],
-                  backgroundImage: const AssetImage('images/me.png'),
-                ),
-                const Text(
-                  "Rahul Kumar",
-                  style: TextStyle(
-                    fontFamily: 'Filxgirl',
-                    fontSize: 40,
-                    letterSpacing: 1,
-                    color: Colors.white,
-                  ),
-                ),
-                const Text(
-                  "FLUTTER DEVELOPER",
-                  style: TextStyle(
-                    fontFamily: 'Ysabeau',
-                    color: Colors.white,
-                    fontSize: 17,
-                    letterSpacing: 3,
-                  ),
-                ),
-                Container(
-                  // for the hr
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundColor: Colors.grey[900],
+                backgroundImage: const AssetImage('images/me.png'),
+              ),
+              const Text(
+                "Rahul Kumar",
+                style: TextStyle(
+                  fontFamily: 'Filxgirl',
+                  fontSize: 40,
+                  letterSpacing: 1,
                   color: Colors.white,
-                  height: 1,
-                  width: 150,
-                  margin: const EdgeInsets.only(
-                    top: 10,
-                  ),
                 ),
-                const Card(
-                  elevation: 7,
+              ),
+              const Text(
+                "FLUTTER DEVELOPER",
+                style: TextStyle(
+                  fontFamily: 'Ysabeau',
                   color: Colors.white,
-                  margin: EdgeInsets.symmetric(
-                    vertical: 10,
-                    horizontal: 25,
+                  fontSize: 17,
+                  letterSpacing: 3,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.grey,
+                ),
+              ),
+              const Card(
+                elevation: 7,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 25,
+                ),
+                // padding: const EdgeInsets.all(7),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    size: 20,
+                    color: Colors.black,
                   ),
-                  // padding: const EdgeInsets.all(7),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.phone,
-                      size: 20,
-                      color: Colors.black,
-                    ),
-                    title: Text(
-                      '+91 9798295265',
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),
+                  title: Text(
+                    '+91 9798295265',
+                    style: TextStyle(
+                      fontSize: 20,
                     ),
                   ),
                 ),
-                const Card(
-                  elevation: 7,
-                  margin: EdgeInsets.symmetric(
-                    vertical: 10,
-                    horizontal: 25,
+              ),
+              const Card(
+                elevation: 7,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 25,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    size: 20,
+                    color: Colors.black,
                   ),
-                  color: Colors.white,
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.email,
-                      size: 20,
-                      color: Colors.black,
-                    ),
-                    title: Text(
-                      'rahulbkba@gmail.com',
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),
+                  title: Text(
+                    'rahulbkba@gmail.com',
+                    style: TextStyle(
+                      fontSize: 20,
                     ),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
